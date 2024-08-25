297521e0c53489e310dabe11f2ee54204b67029c
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-24 19:51:31 -0500

    Auto push

--- init.sh
+++ init.sh
@@ -1,1 +1,8 @@
-ansible-playbook  -i inv-${ENV}  -e ansible_user=ec2-user  -e ansible_password=DevOps321  -e COMPONENT=${COMPONENT}  -e ENV=${ENV} -e PWD=ExpenseApp@1  expense.yml
+if [ "$pipeline" == "true" ] ;then 
+    exit 
+fi 
+
+ansible-playbook -i inv-${ENV} -e ansible_user=ec2-user -e ansible_password=${SSH_PSW} -e COMPONENT=${COMPONENT} -e ENV=${ENV} -e PWD=${MYSQL_PSW} expense.yml
+
+echo "Printing Environment Variables To Detect Secrets If Any"
+env 
